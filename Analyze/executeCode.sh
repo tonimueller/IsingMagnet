0230c6dd8e45a8a5660452a531ad8ea7e0fd76b5
Toni Mueller <mueller-toni@ipfdd.de>
2021-05-03 14:44:24 +0200

    add another data point in executeCode.sh

--- Analyze/executeCode.sh
+++ Analyze/executeCode.sh
@@ -9,14 +9,14 @@
 #importance sampling: 
 samples=20
 N=32
-stepWide=20
+stepWide=40
 nIterations=200
 #total MCS=20*200=4000
 B=0
 cp ../build/IsingMagnet/IsingMagnet  .
 ./IsingMagnet $samples $N $nIterations $stepWide $B
 
-# N=32
+# N=32 stepwide=20
 #                      total    time/(MCS*Sample*size)
 # original Code needs: 192.852s 117.7ns
 # XY-Lookup          : 169.464s 103.433ns
@@ -24,7 +24,9 @@ cp ../build/IsingMagnet/IsingMagnet  .
 # Energy calc with IF: 124.914s 76.2416ns (declined)
 # E_tot for loop opti: 123.033s 75.0933ns (declined)
 
-#N=16
+#N=32 stepwide=40
+# Bitshift Refold    : 249.481s 76.1356ns
+#N=16 stepwide=20
 #                      total    time/(MCS*Sample*size)
 # original Code needs: 
 # XY-Lookup          : 43.2275s 105.536ns
